5424a492e51f7a4a9b9ae3e8c8d6d41d556f1b86
JangamHemaSekharaiah <sekharhema0001@gmail.com>
2024-10-19 14:10:36 +0530

    Working with Maven Commands

---
+++ untitled/src/test/java/mavenpratice/CreateProfile.java
@@ -0,0 +1,21 @@
+package mavenpratice;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class CreateProfile
+{
+
+    @Test
+    public void hello()
+    {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.co.in/");
+    }
+
+
+}
